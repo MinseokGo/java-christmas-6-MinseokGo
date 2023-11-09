f44871710dd5276aa49a1b70b605a42c12a50bd0
MinseokGo <rhalstjr1999@naver.com>
2023-11-10 02:06:15 +0900

    feat(Customer): 할인 금액 계산 기능
    할인 금액을 계산합니다.
    - 크리스마스 디데이 할인을 계산합니다.
    - 평일 할인을 계산합니다.
    - 주말 할인을 계산합니다.
    - 특별 할인을 계산합니다.
    - 증정 이벤트 할인을 계산합니다.

---
+++ src/main/java/christmas/model/Customer.java
@@ -0,0 +1,79 @@
+package christmas.model;
+
+import christmas.model.menu.Menu;
+import christmas.model.menu.Type;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public class Customer {
+    private final int visitDate;
+    private final Order order;
+    private Discount discount;
+
+    public Customer(final int visitDate, final Order order) {
+        this.visitDate = visitDate;
+        this.order = order;
+    }
+
+    public int calculateDDayDiscount() {
+        return (visitDate - 1) * 100 + 1000;
+    }
+
+    public int calculateWeekDayDiscount() {
+        final int dayNumber = getDayNumber();
+        final Map<String, Integer> menus = order.getMenus();
+        if (dayNumber == 5 || dayNumber == 6) {
+            return calculateWeeklyDiscount(menus, Type.MAIN);
+        }
+        return 0;
+    }
+
+    public int calculateWeekendDiscount() {
+        final int dayNumber = getDayNumber();
+        final Map<String, Integer> menus = order.getMenus();
+        if (!(dayNumber == 5) && !(dayNumber == 6)) {
+            return calculateWeeklyDiscount(menus, Type.DESSERT);
+        }
+        return 0;
+    }
+
+    public int calculateSpecialDayDiscount() {
+        final int dayNumber = getDayNumber();
+        if (dayNumber == 7 || visitDate == 25) {
+            return 1000;
+        }
+        return 0;
+    }
+
+    public int calculateGiftDiscount() {
+        return order.getGiftPrice();
+    }
+
+    private int calculateWeeklyDiscount(final Map<String, Integer> menus, final Type type) {
+        final List<String> menuNamesByType = Menu.getMenuByType(type);
+        final int count = countMenu(menus, menuNamesByType);
+        return count * 2023;
+    }
+
+    private int countMenu(final Map<String, Integer> menus, final List<String> menuNamesByType) {
+        List<String> specificMenus = menus.keySet()
+                .stream()
+                .filter(menuNamesByType::contains)
+                .toList();
+        int count = 0;
+        for (String menu : menus.keySet()) {
+            if (specificMenus.contains(menu)) {
+                count += menus.get(menu);
+            }
+        }
+        return count;
+    }
+
+    private int getDayNumber() {
+        final LocalDate date = LocalDate.of(2023, 12, visitDate);
+        final DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.getValue();
+    }
+}
