f77a2e2b0426beb7a1683d7c029164ec81b616d3
MinseokGo <rhalstjr1999@naver.com>
2023-11-09 23:35:37 +0900

    feat(Order): 주문의 총 금액을 얻는 기능

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,9 +1,11 @@
 package christmas.model;
 
+import christmas.model.menu.Menu;
 import java.util.Map;
 
 public class Order {
     private final Map<String, Integer> menus;
+    private int totalPrice;
 
     public Order(final Map<String, Integer> menus) {
         this.menus = menus;
@@ -11,5 +13,14 @@ public class Order {
 
     public Map<String, Integer> getMenus() {
         return menus;
+    }
+
+    public int calculateTotalPrice() {
+        Map<String, Integer> prices = Menu.getPriceOfMenu();
+        menus.keySet()
+                .forEach(menu -> {
+                    totalPrice += prices.get(menu);
+                });
+        return totalPrice;
     }
 }
