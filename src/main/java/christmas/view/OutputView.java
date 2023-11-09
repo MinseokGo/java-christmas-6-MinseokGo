7fc4472b60a216e23f103d5bdcac021bc75ef6ec
MinseokGo <rhalstjr1999@naver.com>
2023-11-10 02:08:30 +0900

    feat(OutputView): 혜택 내역을 출력하는 기능
    할인 금액이 0이 아닐 경우 적용된 혜택의 금액을 출력합니다.

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import christmas.model.Discount;
 import christmas.model.menu.Menu;
 import java.text.DecimalFormat;
 import java.util.Map;
@@ -15,7 +16,7 @@ public class OutputView {
     }
 
     public void eventPreviewMessage(final int visitDate) {
-        System.out.println("12월 " + visitDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+        System.out.println("12월 " + visitDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
     }
 
     public void orderMenu(final Map<String, Integer> menus) {
@@ -25,15 +26,53 @@ public class OutputView {
     }
 
     public void totalOrderPriceBeforeDiscount(final int totalPrice) {
-        System.out.println("<할인 전 총주문 금액>\n" + decimalFormatter(totalPrice));
+        System.out.println("\n<할인 전 총주문 금액>\n" + decimalFormatter(totalPrice));
     }
 
     public void gift(final Menu gift) {
         String result = "없음";
         if (gift == Menu.CHAMPAGNE) {
-            result = "삼페인 1개";
+            result = gift.getName() + "1개";
         }
-        System.out.println("<증정 메뉴>\n" + result);
+        System.out.println("\n<증정 메뉴>\n" + result);
+    }
+
+    public void discountList(final int dDayDiscount,
+                             final int weekDayDiscount,
+                             final int weekendDiscount,
+                             final int specialDayDiscount,
+                             final int giftDiscount) {
+        System.out.println("\n<혜택 내역>");
+        if (dDayDiscount != 0) {
+            System.out.println("크리스마스 디데이 할인: -" + decimalFormatter(dDayDiscount));
+        }
+        if (weekDayDiscount != 0) {
+            System.out.println("평일 할인: -" + decimalFormatter(weekDayDiscount));
+        }
+        if (weekendDiscount != 0) {
+            System.out.println("주말 할인: -" + decimalFormatter(weekendDiscount));
+        }
+        if (specialDayDiscount != 0) {
+            System.out.println("특별 할인: -" + decimalFormatter(specialDayDiscount));
+        }
+        if (giftDiscount != 0) {
+            System.out.println("증정 이벤트: -" + decimalFormatter(giftDiscount));
+        }
+        isAllZeroMoney(dDayDiscount, weekDayDiscount, weekendDiscount, specialDayDiscount, giftDiscount);
+    }
+
+    private void isAllZeroMoney(final int dDayDiscount,
+                                final int weekDayDiscount,
+                                final int weekendDiscount,
+                                final int specialDayDiscount,
+                                final int giftDiscount) {
+        if (dDayDiscount == 0
+                && weekDayDiscount == 0
+                && weekendDiscount == 0
+                && specialDayDiscount == 0
+                && giftDiscount == 0) {
+            System.out.println("없음");
+        }
     }
 
     private String decimalFormatter(final int totalPrice) {
