40301d60aa85f51e430398d86701317d46ff833b
MinseokGo <rhalstjr1999@naver.com>
2023-11-10 02:07:37 +0900

    feat(Discount): 할인 금액을 저장하는 기능
    할인 금액을 저장합니다.

---
+++ src/main/java/christmas/model/Discount.java
@@ -0,0 +1,17 @@
+package christmas.model;
+
+public class Discount {
+    private final int dDay;
+    private final int weekDay;
+    private final int weekend;
+    private final int specialDay;
+    private final int gift;
+
+    public Discount(final int dDay, final int weekDay, final int weekend, final int specialDay, final int gift) {
+        this.dDay = dDay;
+        this.weekDay = weekDay;
+        this.weekend = weekend;
+        this.specialDay = specialDay;
+        this.gift = gift;
+    }
+}
