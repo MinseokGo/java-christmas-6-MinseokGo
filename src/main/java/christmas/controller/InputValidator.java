f4c4f32a7c3e7169e9033ec3505c7866fb039e37
MinseokGo <rhalstjr1999@naver.com>
2023-11-09 22:29:50 +0900

    feat(InputValidator): 입력 메뉴 형식 검증 기능
    "메뉴이름-개수"의 포맷으로 입력하지 않으면 예외처리 합니다.

--- src/main/java/christmas/controller/InputValidator.java
+++ src/main/java/christmas/controller/InputValidator.java
@@ -1,5 +1,12 @@
 package christmas.controller;
 
+import christmas.model.Parser;
+import christmas.model.menu.Menu;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class InputValidator {
     public final String DATE_NOT_VALID_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
@@ -23,6 +30,17 @@ public class InputValidator {
         }
     }
 
+    public boolean orderMenuAndNumberOfMenu(final String input) {
+        try {
+            isStringFormat(input);
+            final Map<String, Integer> menus = Parser.menu(input);
+            return false;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
+    }
+
     private void isNumberFormat(final String input) {
         if (!input.matches("\\d+")) {
             throw new IllegalArgumentException(DATE_NOT_VALID_MESSAGE);
@@ -33,6 +51,12 @@ public class InputValidator {
         final int number = Integer.parseInt(input);
         if (1 > number || number > 31) {
             throw new IllegalArgumentException(DATE_NOT_VALID_MESSAGE);
+        }
+    }
+
+    private void isStringFormat(final String input) {
+        if (!input.matches("^([가-힣]+)-(\\d+)(,[가-힣]+-(\\d+))*$")) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
