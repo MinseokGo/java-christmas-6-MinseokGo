d2de903ff682da6804d27e0cd45370cabc6bc1fc
MinseokGo <rhalstjr1999@naver.com>
2023-11-09 19:51:47 +0900

    feat(InputValidator): 입력이 숫자인지 검증하는 기능
    고객의 입력이 숫자인지 검증합니다. 만약 음수나 숫자가 아닌 문자열을 입력하면 예외 처리합니다.

---
+++ src/main/java/christmas/controller/InputValidator.java
@@ -0,0 +1,30 @@
+package christmas.controller;
+
+public class InputValidator {
+    public final String DATE_NOT_VALID_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    private static final InputValidator inputValidator = new InputValidator();
+
+    private InputValidator() {
+    }
+
+    public static InputValidator getInstance() {
+        return inputValidator;
+    }
+
+    public boolean visitDate(final String input) {
+        try {
+            isNumberFormat(input);
+            return false;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return true;
+        }
+    }
+
+    private void isNumberFormat(final String input) {
+        if (!input.matches("\\d+")) {
+            throw new IllegalArgumentException(DATE_NOT_VALID_MESSAGE);
+        }
+    }
+}
