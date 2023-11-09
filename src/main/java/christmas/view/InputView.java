500edbf7ddb92f4976d4e4b8cbf42b129c85b3a3
MinseokGo <rhalstjr1999@naver.com>
2023-11-09 19:31:02 +0900

    feat(InputView): 날짜 입력 요구 메시지 출력 기능
    간단한 인사와 함께 고객에게 예상 방문 날짜를 묻는 메시지를 출력합니다.

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,17 @@
+package christmas.view;
+
+public class InputView {
+    private static final InputView inputView = new InputView();
+
+    private InputView() {
+    }
+
+    public static InputView getInstance() {
+        return inputView;
+    }
+
+    public void askRestaurantVisitDateMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+    }
+}
