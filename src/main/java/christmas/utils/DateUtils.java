6e782bdd58ea904ff3bdc2a5166b27b0938a3e80
MinseokGo <rhalstjr1999@naver.com>
2023-11-11 12:11:20 +0900

    feat(DataUtils): 날짜 상수 추가

--- src/main/java/christmas/utils/DateUtils.java
+++ src/main/java/christmas/utils/DateUtils.java
@@ -10,6 +10,8 @@ public class DateUtils {
     public static final int CHRISTMAS_DAY = 25;
     public static final int THIS_YEAR = 2023;
     public static final int THIS_MONTH = 12;
+    public static final int EVENT_START_DATE = 1;
+    public static final int EVENT_END_DATE = 31;
 
 
     public static int getDayNumber(final int visitDate) {
