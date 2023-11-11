f50ad59b9b7e430ca528f34dfb48eb2f235fe18e
MinseokGo <rhalstjr1999@naver.com>
2023-11-11 11:52:11 +0900

    feat(DateUtils): 날짜 관련 유틸 기능 추가

---
+++ src/main/java/christmas/utils/DateUtils.java
@@ -0,0 +1,20 @@
+package christmas.utils;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class DateUtils {
+    public static final int FRIDAY = 5;
+    public static final int SATURDAY = 6;
+    public static final int SUNDAY = 7;
+    public static final int CHRISTMAS_DAY = 25;
+    public static final int THIS_YEAR = 2023;
+    public static final int THIS_MONTH = 12;
+
+
+    public static int getDayNumber(final int visitDate) {
+        final LocalDate date = LocalDate.of(DateUtils.THIS_YEAR, THIS_MONTH, visitDate);
+        final DayOfWeek dayOfWeek = date.getDayOfWeek();
+        return dayOfWeek.getValue();
+    }
+}
