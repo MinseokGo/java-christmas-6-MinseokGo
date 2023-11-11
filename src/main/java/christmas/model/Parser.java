6364d73bd34c70449aca33e30bdab8a59ec26c8d
MinseokGo <rhalstjr1999@naver.com>
2023-11-11 12:18:23 +0900

    refactor(Parser): 파싱 로직 분리

--- src/main/java/christmas/model/Parser.java
+++ src/main/java/christmas/model/Parser.java
@@ -1,24 +1,25 @@
 package christmas.model;
 
 import christmas.controller.ParseValidator;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Parser {
     public static Map<String, Integer> menu(final String input) {
-        Map<String, Integer> menus = new HashMap<>();
-        List<String> splitInput = List.of(input.split(","));
-        splitInput.stream()
+        final Map<String, Integer> menus = new HashMap<>();
+        Arrays.stream(input.split(","))
                 .map(item -> item.split("-"))
-                .forEach(menu -> {
-                    final String name = menu[0];
-                    final int quantity = Integer.parseInt(menu[1]);
-                    ParseValidator.isSmallerThanOne(quantity);
-                    ParseValidator.isDuplicateMenu(menus, name);
-                    menus.put(name, quantity);
-                });
+                .forEach(menu -> processMenuEntry(menu, menus));
         ParseValidator.isOnlyDrink(menus);
         return menus;
+    }
+
+    private static void processMenuEntry(String[] menu, Map<String, Integer> menus) {
+        final String name = menu[0];
+        final int quantity = Integer.parseInt(menu[1]);
+        ParseValidator.isSmallerThanOne(quantity);
+        ParseValidator.isDuplicateMenu(menus, name);
+        menus.put(name, quantity);
     }
 }
