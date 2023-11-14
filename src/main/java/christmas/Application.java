8f1d050f71373b6b865946a8589e0423de69d2ed
MinseokGo <rhalstjr1999@naver.com>
2023-11-14 19:11:38 +0900

    refactor(Application): Discount 의존 주입

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,11 +1,12 @@
 package christmas;
 
 import christmas.controller.EventController;
+import christmas.model.Discount;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        EventController eventController = new EventController();
+        EventController eventController = new EventController(new Discount());
         eventController.inputVisitDate();
         eventController.inputOrderMenuAndNumberOfMenu();
         eventController.orderPreview();
